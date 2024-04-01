206edd0777f8ade4d62f45d76e0f936b44f6809e
pankazillion <kppankaj1843@gmail.com>
2024-04-01 19:01:10 +0530

    completed schemas and routes

--- server/db.js
+++ server/db.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 
+mongoose.connect("mongodb://localhost:27017/paytm")
+
 // Create a Schema for Users
 const userSchema = new mongoose.Schema({
     username: {
@@ -30,9 +32,22 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-// Create a model from the schema
+const accountSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId, // Reference to User model
+        ref: 'User',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
+});
+
+const Account = mongoose.model('Account', accountSchema);
 const User = mongoose.model('User', userSchema);
 
 module.exports = {
-	User
+	User,
+  Account,
 };
--- server/index.js
+++ server/index.js
@@ -3,4 +3,14 @@ const rootRouter = require("./routes/index");
 
 const app = express();
 
+const port = 5001;
+
+app.get('/', (req, res) => {
+    res.send("<h1>Hello World</h1>")
+})
+
 app.use("/api/v1", rootRouter);
+
+app.listen(port, ()=>{
+    console.log(`server is up and running on port ${5001}`);
+})
